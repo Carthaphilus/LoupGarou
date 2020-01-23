7ef7cb35d68937e0a23156d8a2042154097b29ad
Jeremy R <robertjeremy19068@gmail.com>
2020-01-23 10:08:41 +0400

    Modif 1.0.9

--- src/loup/garou/chooseServer.java
+++ src/loup/garou/chooseServer.java
@@ -119,7 +119,8 @@ public class chooseServer extends javax.swing.JFrame implements Trucable {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         connexion = new Serveur(ipServeur.getText(), this);
-        connexion.Envoie(inputName.getText());     
+        connexion.Envoie(inputName.getText()); 
+        connexion.Recevoir();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -168,7 +169,6 @@ public class chooseServer extends javax.swing.JFrame implements Trucable {
     // End of variables declaration//GEN-END:variables
 
     public void etat(Object Message) {
-        connexion.Recevoir();  
         jLabel2.setText((String) Message);
         System.out.println((String) Message);
     }
